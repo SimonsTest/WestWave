9b99268b3f854942fb2709eaae27bc59513c9c5d
SimonsTest <drastilsimon@gmail.com>
2025-01-16 00:05:15 -0600

    Delete loader.js

--- loader.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    // Set the duration for the loader animation
-    setTimeout(function() {
-        // Redirect to the home page after the loader finishes
-        window.location.href = 'home.html'; // Change to the correct home page URL
-    }, 3000);  // Duration of the loader in milliseconds (3 seconds in this case)
-};
